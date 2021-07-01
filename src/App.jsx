a12e9b4449d9c2dc3e862e1c01cdb6c57b265e13
Marcelino Teixeira <marceometry@gmail.com>
2021-07-01 20:39:22 -0300

    start building html

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,66 @@
-import './App.css';
+import './App.css'
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <h1>Toma</h1>
-    </div>
-  );
-}
+      <header>
+        <form>
+          <input type="select" name="location" />
+          <input type="select" name="date" />
+        </form>
 
-export default App;
+        <button>Dark Theme</button>
+      </header>
+    
+      <main>
+        <div className="main-forecast">
+          <h1>Uyo, Nigeria - 01/18/2021</h1>
+
+          <div className="forecast-illustration"></div>
+
+          <span>Cloudy, <strong>29.5 C</strong></span>
+          <span>Change of rain: <strong>80%</strong></span>
+        </div>
+        <div>
+          <section>
+            <div className="section-row">
+              <p>
+                <span>Humidity:</span>
+                <strong>77%</strong>
+              </p>
+              <img src="" alt=":p" />
+            </div>
+            <div className="section-row">
+              <p>
+                <span>Pressure:</span>
+                <strong>1000.7 mb</strong>
+              </p>
+              <img src="" alt=":p" />
+            </div>
+            <div className="section-row">
+              <p>
+                <span>Wind:</span>
+                <strong>WSW at 5 m/s</strong>
+              </p>
+              <img src="" alt=":p" />
+            </div>
+          </section>
+          
+          <section>
+            <div className="section-row">
+              <p>
+                <span>Soil Moisture:</span>
+                <strong>10 cm</strong>
+              </p>
+              <img src="" alt=":p" />
+            </div>
+
+            <div className="flood-risk">
+              ! High flood risk
+            </div>
+          </section>
+        </div>
+      </main>
+    </div>
+  )
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import GlobalStyle from './styles/globalStyle'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyle>
+      <App />
+    </GlobalStyle>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
