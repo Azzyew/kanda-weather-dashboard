bc776f70b2e8714c6131cadde6e103bc7733334d
azzyew <laisadnevescosta@gmail.com>
2021-07-02 15:16:00 -0300

    changing style to styled-components

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import './styles/global.css'
-// import GlobalStyle from './styles/globalStyle'
+import GlobalStyle from './styles/globalStyle'
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <GlobalStyle> */}
+    <GlobalStyle />
       <App />
-    {/* </GlobalStyle> */}
   </React.StrictMode>,
   document.getElementById('root')
 )
