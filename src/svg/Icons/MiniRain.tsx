c504e1382dd84e9aa64fbed1de83d407e823d8c7
Marcelino Teixeira <marceometry@gmail.com>
2021-07-03 20:57:37 -0300

    create svg components

---
+++ src/svg/Icons/MiniRain.tsx
@@ -0,0 +1,7 @@
+export function MiniRain() {
+    return (
+        <svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M16 50.7846L26 33.4641M36 46.7846L46 29.4641M52 54.7846L62 37.4641" stroke="#1FA4EF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+    )
+}
