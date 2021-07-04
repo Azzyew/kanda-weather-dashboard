563bfdaf767d8218c69b77eb56113dbc9f465f17
Marcelino Teixeira <marceometry@gmail.com>
2021-07-03 21:40:51 -0400

    Update svg imports

--- src/components/FloodRisk/index.jsx
+++ src/components/FloodRisk/index.jsx
@@ -1,5 +1,5 @@
-import Danger from '../../svg/icons/Danger.svg'
-import Check from '../../svg/icons/Check.svg'
+import Danger from '../../svg/icons/Danger'
+import Check from '../../svg/icons/Check'
 import StyledFloodRisk from './style.js'
 
 export function FloodRisk({ risk = false }) {
