97612b288969ed73e8159e6ec28b8b951bd05cbd
azzyew <laisadnevescosta@gmail.com>
2021-07-30 01:06:19 -0300

    adding api data in json

--- src/components/OtherInfo/SoilMoistureContainer/index.jsx
+++ src/components/OtherInfo/SoilMoistureContainer/index.jsx
@@ -1,13 +1,30 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react"
 import { useTranslation } from "react-i18next"
+// import { dClimateAPI, soilMoistureEndpoint } from "../../../data/api"
 import { SoilMoisture } from "../../../svg/icons/SoilMoisture"
 import { FloodRisk } from "./FloodRisk"
 
 export function SoilMoistureContainer({ soilMoisture }) {
   const { t } = useTranslation()
-  const [floodRisk, setFloodRisk] = useState()
+  const [floodRisk, setFloodRisk] = useState();
+  // const [soilM, setSoilM] = useState();
 
   useEffect(() => {
+    // (async () => {
+    //   await axios.get('https://api.dclimate.net/apiv2/grid-history/era5_volumetric_soil_water_layer_1-hourly/5.68999972_-0.20985164?also_return_metadata=false&use_imperial_units=true&also_return_snapped_coordinates=true&convert_to_local_time=true', {
+    //     headers: {
+    //       'Access-Control-Allow-Origin': '*'
+    //     }
+    //   }).then((response) => {
+    //     if(response.data){
+    //       setSoilM(response.data);
+    //     }
+    //   }).catch((error) => {
+    //     console.log("Erro da request: ");
+    //     console.log(error);
+    //   })
+    // })();
+
     setFloodRisk(parseFloat(soilMoisture) > 10)
   }, [soilMoisture])
 
